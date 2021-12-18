e636a3e875094d3782c137cda95b1641f0e969a2
Kole <novoselkosta@gmail.com>
2021-12-18 18:42:09 +0100

    Added BouncyProjectile

---
+++ code/entities/projectiles/BouncyProjectile.cs
@@ -0,0 +1,28 @@
+﻿using Sandbox;
+
+namespace PaintBall
+{
+	public class BouncyProjectile : Projectile
+	{
+		public float Bounciness { get; set; } = 0.8f;
+		public override float LifeTime => 5f;
+
+		protected override bool HasHitTarget( TraceResult trace )
+		{
+			if ( trace.Hit )
+			{
+				if ( trace.Entity is not WorldEntity )
+					return true;
+
+				var reflect = Vector3.Reflect( Velocity.Normal, trace.Normal );
+
+				GravityModifier = 0f;
+				Velocity = reflect * Velocity.Length * Bounciness;
+
+				return false;
+			}
+
+			return base.HasHitTarget( trace );
+		}
+	}
+}
