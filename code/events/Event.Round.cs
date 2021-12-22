899540cce73559885a2f7822fbf3d00a17f1b4d8
Kole <novoselkosta@gmail.com>
2021-12-22 20:17:45 +0100

    Added Round events

---
+++ code/events/Event.Round.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+
+namespace PaintBall
+{
+	public static partial class PBEvent
+	{
+		public static class Round
+		{
+			public const string Start = "pb.round.start";
+
+			public class StartAttribute : EventAttribute
+			{
+				public StartAttribute() : base( Start ) { }
+			}
+
+			public const string End = "pb.round.end";
+
+			public class EndAttribute : EventAttribute
+			{
+				public EndAttribute() : base( End ) { }
+			}
+		}
+	}
+}
