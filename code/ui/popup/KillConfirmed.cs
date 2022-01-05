bb29ecf03be25c3a4b27d15af741660674e9270a
Kole <novoselkosta@gmail.com>
2022-01-05 16:18:52 +0100

    KillConfiremd.cs now shows the HitIndex of the player killed

--- code/ui/popup/KillConfirmed.cs
+++ code/ui/popup/KillConfirmed.cs
@@ -14,6 +14,7 @@ namespace PaintBall
 			StyleSheet.Load( "/ui/popup/KillConfirmed.scss" );
 		}
 
+
 		[PBEvent.Player.Killed]
 		public void OnPlayerKilled( Player player, Entity attacker )
 		{
@@ -22,8 +23,9 @@ namespace PaintBall
 
 			_currentPopUp?.Delete();
 			_currentPopUp = AddChild<KillConfirmedEntry>();
-			_currentPopUp.Name.Text = $"You killed {player.Client.Name}";
+			_currentPopUp.Name.Text = $"YOU KILLED {player.Client.Name.ToUpper()}";
 			_currentPopUp.Icon.SetTexture( $"avatar:{player.Client.PlayerId}" );
+			_currentPopUp.SetHit( (HitboxGroup)player.GetHitboxGroup( player.LastHitboxIndex ) );
 		}
 
 		[PBEvent.Round.New]
@@ -36,13 +38,56 @@ namespace PaintBall
 		{
 			public Image Icon { get; set; }
 			public Label Name { get; set; }
+			private Image _head;
+			private Image _chest;
+			private Image _stomach;
+			private Image _leftArm;
+			private Image _rightArm;
+			private Image _leftLeg;
+			private Image _rightLeg;
 
 			public KillConfirmedEntry()
 			{
 				Icon = Add.Image( "", "icon" );
 				Name = Add.Label( "", "name" );
+				_head = Add.Image( "ui/terry/head.png", "terry" );
+				_chest = Add.Image( "ui/terry/stomach.png", "terry" );
+				_stomach = _chest;
+				_leftArm = Add.Image( "ui/terry/leftarm.png", "terry" );
+				_rightArm = Add.Image( "ui/terry/rightarm.png", "terry" );
+				_leftLeg = Add.Image( "ui/terry/leftleg.png", "terry" );
+				_rightLeg = Add.Image( "ui/terry/rightleg.png", "terry" );
 
 				_ = DeleteAsync();
+			}
+
+			public void SetHit( HitboxGroup hitboxGroup )
+			{
+				Log.Info( hitboxGroup );
+				switch ( hitboxGroup )
+				{
+					case HitboxGroup.Head:
+						_head.SetClass( "hit", true );
+						return;
+					case HitboxGroup.Chest:
+						_chest.SetClass( "hit", true );
+						return;
+					case HitboxGroup.Stomach:
+						_stomach.SetClass( "hit", true );
+						return;
+					case HitboxGroup.LeftArm:
+						_leftArm.SetClass( "hit", true );
+						return;
+					case HitboxGroup.RightArm:
+						_rightArm.SetClass( "hit", true );
+						return;
+					case HitboxGroup.LeftLeg:
+						_leftLeg.SetClass( "hit", true );
+						return;
+					case HitboxGroup.RightLeg:
+						_rightLeg.SetClass( "hit", true );
+						return;
+				}
 			}
 
 			private async Task DeleteAsync()
