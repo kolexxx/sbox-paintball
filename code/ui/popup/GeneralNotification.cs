e769a4903b2d03cd2dd7196b1fb57173be9a3c1e
Kole <novoselkosta@gmail.com>
2022-01-05 16:18:58 +0100

    Mark up

---
+++ code/ui/popup/GeneralNotification.cs
@@ -0,0 +1,31 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace PaintBall
+{
+	public class GeneralNotification : Panel
+	{
+		public Label Message;
+		private Panel _currentPopUp;
+
+		public GeneralNotification() { }
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			if ( _currentPopUp == null )
+				return;
+		}
+
+		public void AddMessage( string text )
+		{
+			if ( string.IsNullOrWhiteSpace( text ) )
+				return;
+
+			_currentPopUp = Add.Panel();
+			Message = _currentPopUp.Add.Label( text );
+		}
+	}
+}
