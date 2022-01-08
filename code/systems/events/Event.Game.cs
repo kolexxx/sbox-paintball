e109b4f243aa4711b46fae58a170d93aae21f222
Kole <novoselkosta@gmail.com>
2022-01-08 16:07:55 +0100

    Added Game events

---
+++ code/systems/events/Event.Game.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox;
+
+namespace PaintBall
+{
+	public static partial class PBEvent
+	{
+		public static class Game
+		{
+			public const string StateChanged = "pb.state.changed";
+
+			/// <summary>
+			/// Runs when the Game state changes.
+			/// <para>Event is passed the <strong><see cref="PaintBall.BaseState"/></strong> instance of the old state
+			/// and the <strong><see cref="PaintBall.BaseState"/></strong> instance of the new state.</para>
+			/// </summary>
+			public class StateChangedAttribute : EventAttribute
+			{
+				public StateChangedAttribute() : base( StateChanged ) { }
+			}
+		}
+	}
+}
