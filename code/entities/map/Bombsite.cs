e3af8afb732a1f1ebde3f10cd9ffa5ea8d1eab33
Kole <novoselkosta@gmail.com>
2022-01-11 01:40:43 +0100

    Markup for bombsite

---
+++ code/entities/map/Bombsite.cs
@@ -0,0 +1,12 @@
+﻿using Sandbox;
+
+namespace PaintBall
+{
+	[Library( "pb_bombsite", Description = "Place where the Red Team can plant the bomb." )]
+	[Hammer.AutoApplyMaterial( "materials/tools/toolstrigger.vmat" )]
+	[Hammer.Solid]
+	[Hammer.VisGroup( Hammer.VisGroup.Trigger )]
+	public class Bombsite : BaseTrigger
+	{
+	}
+}
