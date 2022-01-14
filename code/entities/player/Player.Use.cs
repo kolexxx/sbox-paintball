8beb2a138203719123c36fee98b242b178d5105b
Kole <novoselkosta@gmail.com>
2022-01-14 19:01:11 +0100

    Implement Use button

---
+++ code/entities/player/Player.Use.cs
@@ -0,0 +1,61 @@
+﻿using Sandbox;
+
+namespace PaintBall
+{
+	public partial class Player
+	{
+		public bool IsUseDisabled()
+		{
+			return ActiveChild is IUse use && use.IsUsable( this );
+		}
+
+		protected override Entity FindUsable()
+		{
+			if ( IsUseDisabled() )
+				return null;
+
+			// First try a direct 0 width line
+			var tr = Trace.Ray( EyePos, EyePos + EyeRot.Forward * (105 * Scale) )
+				.HitLayer( CollisionLayer.All )
+				.Ignore( this )
+				.Run();
+
+			// See if any of the parent entities are usable if we ain't.
+			var ent = tr.Entity;
+			while ( ent.IsValid() && !IsValidUseEntity( ent ) )
+			{
+				ent = ent.Parent;
+			}
+
+			// Nothing found, try a wider search
+			if ( !IsValidUseEntity( ent ) )
+			{
+				tr = Trace.Ray( EyePos, EyePos + EyeRot.Forward * (105 * Scale) )
+				.Radius( 2 )
+				.HitLayer( CollisionLayer.All )
+				.Ignore( this )
+				.Run();
+
+				// See if any of the parent entities are usable if we ain't.
+				ent = tr.Entity;
+				while ( ent.IsValid() && !IsValidUseEntity( ent ) )
+				{
+					ent = ent.Parent;
+				}
+			}
+
+			// Still no good? Bail.
+			if ( !IsValidUseEntity( ent ) ) return null;
+
+			return ent;
+		}
+
+		protected override void UseFail()
+		{
+			if ( IsUseDisabled() )
+				return;
+
+			base.UseFail();
+		}
+	}
+}
