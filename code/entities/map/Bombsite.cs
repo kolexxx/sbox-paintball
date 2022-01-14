d21558d78224feed8df0e96a3e07bb077af00183
Kole <novoselkosta@gmail.com>
2022-01-14 19:00:10 +0100

    Update Bombsite.cs

--- code/entities/map/Bombsite.cs
+++ code/entities/map/Bombsite.cs
@@ -6,7 +6,34 @@ namespace PaintBall
 	[Hammer.AutoApplyMaterial( "materials/tools/toolstrigger.vmat" )]
 	[Hammer.Solid]
 	[Hammer.VisGroup( Hammer.VisGroup.Trigger )]
-	public class Bombsite : BaseTrigger
+	public partial class Bombsite : BaseTrigger
 	{
+		[Net, Property] public char Letter { get; set; }
+
+		public override void Spawn()
+		{
+			base.Spawn();
+
+			Transmit = TransmitType.Always;
+		}
+
+		public override void OnTouchStart( Entity toucher )
+		{
+			base.OnTouchStart( toucher );
+
+			(toucher as Player).IsOnBombsite = true;
+		}
+
+		public override void OnTouchEnd( Entity toucher )
+		{
+			base.EndTouch( toucher );
+
+			(toucher as Player).IsOnBombsite = false;
+		}
+
+		public override bool PassesTriggerFilters( Entity other )
+		{
+			return other is Player player && player.Alive();
+		}
 	}
 }
