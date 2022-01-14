2c2b38c930988cd05d9350b0002bc1959863e92f
Kole <novoselkosta@gmail.com>
2022-01-14 19:00:30 +0100

    Added exclusive check, DropBomb function

--- code/entities/player/Inventory.cs
+++ code/entities/player/Inventory.cs
@@ -7,12 +7,21 @@ namespace PaintBall
 	{
 		public Inventory( Player player ) : base( player ) { }
 
+		public new Player Owner
+		{
+			get => base.Owner as Player;
+			init => base.Owner = value;
+		}
+
 		public override bool Add( Entity entity, bool makeActive = false )
 		{
 			if ( entity is not Weapon weapon )
 				return false;
 
-			if ( List.Any( x => (x as Weapon).Bucket == weapon.Bucket ) )
+			if ( weapon.ExclusiveFor != Team.None && weapon.ExclusiveFor != Owner.Team )
+				return false;
+
+			if ( List.Any( x => (x as Weapon).Slot == weapon.Slot ) )
 				return false;
 
 			return base.Add( entity, makeActive );
@@ -26,6 +35,17 @@ namespace PaintBall
 				return null;
 
 			return base.DropActive();
+		}
+
+		public Bomb DropBomb()
+		{
+			if ( Owner.Team != Team.Red )
+				return null;
+
+			var bomb = List.Find( x => x is Bomb ) as Bomb;
+			Drop( bomb );
+
+			return bomb;
 		}
 	}
 }
