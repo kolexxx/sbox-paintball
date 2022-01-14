7b6bf2ba6d1af27e3e5ee4b1d9565ed36e615d54
Kole <novoselkosta@gmail.com>
2022-01-14 18:59:11 +0100

    Create ProgressBar.cs

---
+++ code/ui/components/progressbar/ProgressBar.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using System;
+
+namespace PaintBall
+{
+	public class ProgressBar : Panel
+	{
+		public bool DeleteOnComplete { get; init; }
+		public Func<float> Percentage { get; init; }
+
+		public ProgressBar( Func<float> percentage, bool deleteOnComplete )
+		{
+			DeleteOnComplete = deleteOnComplete;
+			Percentage = percentage;
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+			if ( !IsVisible )
+				return;
+
+			float result = Percentage();
+
+			Style.Width = Length.Percent( result );
+
+			if ( result >= 1f && DeleteOnComplete )
+				Delete();
+		}
+	}
+}
