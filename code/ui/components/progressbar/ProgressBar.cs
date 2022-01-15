f8a8da00ed3c64a8d43a646b761d146be7b41982
Kole <novoselkosta@gmail.com>
2022-01-15 21:29:16 +0100

    Update ProgressBar.cs

--- code/ui/components/progressbar/ProgressBar.cs
+++ code/ui/components/progressbar/ProgressBar.cs
@@ -4,15 +4,26 @@ using System;
 
 namespace PaintBall
 {
+	/// <summary>
+	/// A <strong><see cref="Sandbox.UI.Panel"></see></strong> that changes width
+	/// based on the result of a function. The function should return a value
+	/// between 0 and 1.
+	/// </summary>
 	public class ProgressBar : Panel
 	{
 		public bool DeleteOnComplete { get; init; }
-		public Func<float> Percentage { get; init; }
+		/// <summary>
+		/// Function that returns a float between 0 and 1.
+		/// </summary>
+		public Func<float> Fraction { get; init; }
+		private RealTimeSince _sinceGetPercentage = 0.25f;
+		private Panel _innerPanel;
 
-		public ProgressBar( Func<float> percentage, bool deleteOnComplete )
+		public ProgressBar( Func<float> fraction, bool deleteOnComplete = false )
 		{
 			DeleteOnComplete = deleteOnComplete;
-			Percentage = percentage;
+			Fraction = fraction;
+			_innerPanel = Add.Panel( "inner" );
 		}
 
 		public override void Tick()
@@ -22,9 +33,14 @@ namespace PaintBall
 			if ( !IsVisible )
 				return;
 
-			float result = Percentage();
+			if ( _sinceGetPercentage < 0.2f )
+				return;
 
-			Style.Width = Length.Percent( result );
+			_sinceGetPercentage = 0;
+
+			float result = Fraction();
+
+			_innerPanel.Style.Width = Length.Fraction( result );
 
 			if ( result >= 1f && DeleteOnComplete )
 				Delete();
