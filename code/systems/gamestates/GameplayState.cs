5e7697e727f5ba63271bb613dc1d7bb6f1e6d3d9
Kole <novoselkosta@gmail.com>
2022-01-15 21:29:55 +0100

    Changed RoundState name, implemented new Bomb state

--- code/systems/gamestates/GameplayState.cs
+++ code/systems/gamestates/GameplayState.cs
@@ -4,6 +4,15 @@ using System.Linq;
 
 namespace PaintBall
 {
+	public enum RoundState : byte
+	{
+		None,
+		Freeze,
+		Play,
+		Bomb,
+		End
+	}
+
 	public partial class GameplayState : BaseState
 	{
 		[ServerVar( "pb_freeze_duration", Help = "The duration of the freeze period." )]
@@ -14,25 +23,19 @@ namespace PaintBall
 		public static int EndDuration { get; set; } = 5;
 		[ServerVar( "pb_bomb_duration", Help = "The time needed for the bomb to explode." )]
 		public static int BombDuration { get; set; } = 30;
+		[ServerVar( "pb_round_limit", Help = "The amount of rounds." )]
+		public static int RoundLimit { get; set; } = 12;
+
 		[Net, Change] public int AliveBlue { get; private set; } = 0;
 		[Net, Change] public int AliveRed { get; private set; } = 0;
 		[Net] public int BlueScore { get; private set; } = 0;
 		[Net] public int RedScore { get; private set; } = 0;
-		[Net] public PlantedBomb Bomb { get; set; }
-		[Net, Change] public RoundState CurrentRoundState { get; private set; }
-		public override bool UpdateTimer => CurrentRoundState != RoundState.End;
+		[Net, Change] public RoundState RoundState { get; set; }
+		public PlantedBomb Bomb { get; set; }
+		public override bool UpdateTimer => RoundState != RoundState.End;
 		private bool _firstBlood = false;
-		private int _roundLimit => 12;
 		private int _toWinScore => 7;
 		private int _round = 0;
-
-		public enum RoundState : byte
-		{
-			None,
-			Freeze,
-			Play,
-			End
-		}
 
 		public override void OnPlayerLeave( Player player )
 		{
@@ -93,7 +96,7 @@ namespace PaintBall
 		{
 			base.Tick();
 
-			switch ( CurrentRoundState )
+			switch ( RoundState )
 			{
 				case RoundState.Freeze:
 
@@ -101,8 +104,23 @@ namespace PaintBall
 
 				case RoundState.Play:
 
-					if ( Host.IsServer && (AliveBlue == 0 || (!Bomb.IsValid() && AliveRed == 0)) )
+					if ( !Host.IsServer )
+						break;
+
+					if ( AliveBlue == 0 || AliveRed == 0 )
 						RoundStateFinish();
+
+					break;
+
+				case RoundState.Bomb:
+
+					if ( !Host.IsServer )
+						break;
+
+					if ( AliveBlue == 0 || Bomb.Disabled )
+						RoundStateFinish();
+
+					Bomb.Tick();
 
 					break;
 
@@ -110,8 +128,6 @@ namespace PaintBall
 
 					break;
 			}
-
-			Bomb?.Tick();
 		}
 
 		public override void Start()
@@ -121,17 +137,20 @@ namespace PaintBall
 			foreach ( var player in Players )
 				player.Reset();
 
-			CurrentRoundState = RoundState.Freeze;
+			RoundState = RoundState.Freeze;
 
 			if ( Host.IsServer )
 				RoundStateStart();
 		}
 
-		private void RoundStateStart()
+		public void RoundStateStart()
 		{
-			switch ( CurrentRoundState )
+			switch ( RoundState )
 			{
 				case RoundState.Freeze:
+
+					Bomb?.Delete();
+					Bomb = null;
 
 					_firstBlood = false;
 					FreezeTime = 5f;
@@ -173,6 +192,14 @@ namespace PaintBall
 
 					break;
 
+				case RoundState.Bomb:
+
+					Event.Run( PBEvent.Round.Bomb.Planted, Bomb );
+
+					StateEndTime = BombDuration + Time.Now;
+
+					break;
+
 				case RoundState.End:
 
 					Event.Run( PBEvent.Round.End, GetWinner() );
@@ -183,28 +210,31 @@ namespace PaintBall
 			}
 
 			// Call OnSecond() as soon as RoundState starts
-			NextSecondTime = 0f;
 		}
 
-		private void RoundStateFinish()
+		public void RoundStateFinish()
 		{
-			switch ( CurrentRoundState )
+			switch ( RoundState )
 			{
 				case RoundState.Freeze:
 
-					CurrentRoundState = RoundState.Play;
+					RoundState = RoundState.Play;
 
 					break;
 
 				case RoundState.Play:
 
-					CurrentRoundState = RoundState.End;
+					RoundState = RoundState.End;
 
-					Team winner = GetWinner();
+					_ = GetWinner() == Team.Blue ? BlueScore++ : RedScore++;
 
-					Audio.AnnounceAll( $"{winner.GetString()}win", Audio.Priority.High );
+					break;
 
-					_ = winner == Team.Blue ? BlueScore++ : RedScore++;
+				case RoundState.Bomb:
+
+					RoundState = RoundState.End;
+
+					_ = GetWinner() == Team.Blue ? BlueScore++ : RedScore++;
 
 					break;
 
@@ -212,14 +242,14 @@ namespace PaintBall
 
 					_round++;
 
-					if ( BlueScore == _toWinScore || RedScore == _toWinScore || _round == _roundLimit )
+					if ( BlueScore == _toWinScore || RedScore == _toWinScore || _round >= RoundLimit )
 					{
 						Game.Current.ChangeState( new GameFinishedState() );
 
 						return;
 					}
 
-					CurrentRoundState = RoundState.Freeze;
+					RoundState = RoundState.Freeze;
 
 					AliveBlue = 0;
 					AliveRed = 0;
@@ -228,31 +258,6 @@ namespace PaintBall
 			}
 
 			RoundStateStart();
-		}
-
-		public void BombPlanted()
-		{
-			if ( CurrentRoundState == RoundState.Play )
-			{
-				StateEndTime = Time.Now + 30f;
-				NextSecondTime = 0f;
-			}
-		}
-
-		public void BombExplode()
-		{
-			if ( CurrentRoundState == RoundState.Play )
-			{
-				RoundStateFinish();
-			}
-		}
-
-		public void BombDefused()
-		{
-			if ( CurrentRoundState == RoundState.Play )
-			{
-				RoundStateFinish();
-			}
 		}
 
 		private void AdjustTeam( Team team, int num )
@@ -276,7 +281,7 @@ namespace PaintBall
 		{
 			if ( Bomb.IsValid() )
 			{
-				if ( Bomb.Defused )
+				if ( Bomb.Defuser != null )
 					return Team.Blue;
 				else
 					return Team.Red;
@@ -291,7 +296,7 @@ namespace PaintBall
 				return Team.Red;
 		}
 
-		private void OnCurrentRoundStateChanged( RoundState oldState, RoundState newState )
+		private void OnRoundStateChanged( RoundState oldState, RoundState newState )
 		{
 			switch ( newState )
 			{
@@ -304,6 +309,12 @@ namespace PaintBall
 				case RoundState.Play:
 
 					Event.Run( PBEvent.Round.Start );
+
+					return;
+
+				case RoundState.Bomb:
+
+					Event.Run( PBEvent.Round.Bomb.Planted, Bomb );
 
 					return;
 
