dbe41b4c64bd48b51a0fb36492abb5ca48cb09e4
Kole <novoselkosta@gmail.com>
2022-01-17 22:42:32 +0100

    Markup

---
+++ code/entities/player/camera/FixedSpectateCamera.cs
@@ -0,0 +1,13 @@
+﻿using Sandbox;
+
+namespace PaintBall;
+
+public class FixedSpectateCamera : Camera, ISpectateCamera
+{
+	public override void Update()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public void OnSpectatedPlayerChanged( Player oldPlayer, Player newPlayer ) { }
+}
