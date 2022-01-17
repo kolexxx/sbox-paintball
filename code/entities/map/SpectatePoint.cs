b3c162f1a73359ed9133e7c44ad070421a2f9b0c
Kole <novoselkosta@gmail.com>
2022-01-17 22:42:36 +0100

    Markup

---
+++ code/entities/map/SpectatePoint.cs
@@ -0,0 +1,10 @@
+﻿using Sandbox;
+
+namespace PaintBall;
+
+[Library( "pb_spectatepoint" )]
+[Hammer.EntityTool("Spectate Point", "PaintBall", "Defines a fixed point where a player can spectate from." )]
+public partial class SpectatePoint : Entity
+{
+
+}
