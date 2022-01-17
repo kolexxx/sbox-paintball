66fceea6dab99777026676b06dbd645ddb79b0ae
Kole <novoselkosta@gmail.com>
2022-01-17 22:42:01 +0100

    Rename

--- code/entities/map/MapCamera.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sandbox;
-
-namespace PaintBall;
-
-[Hammer.EntityTool("Map Camera", "PaintBall", "Defines a fixed point where a player can spectate from." )]
-public partial class MapCamera : Entity
-{
-
-}
