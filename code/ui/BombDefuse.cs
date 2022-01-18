9ebbb8b4279580535c916167380669e911e6f078
Kole <novoselkosta@gmail.com>
2022-01-19 00:15:27 +0100

    Added Panel popup when you are defusing a bomb

---
+++ code/ui/BombDefuse.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace PaintBall;
+
+public class BombDefuse : Panel
+{
+	public Label Instructions;
+	public ProgressBar ProgressBar;
+
+	public BombDefuse()
+	{
+		StyleSheet.Load( "/ui/BombDefuse.scss" );
+
+		Instructions = Add.Label( "Keep holding E to defuse", "instructions" );
+		AddChild( new ProgressBar( () =>
+		  {
+			  var bomb = (Game.Current.State as GameplayState).Bomb;
+			  return (float)bomb.TimeSinceStartedBeingDefused.Relative / 5f;
+		  } ) );
+
+		ProgressBar = GetChild( ChildrenCount - 1 ) as ProgressBar;
+	}
+}
