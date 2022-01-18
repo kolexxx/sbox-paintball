76f79aaa2fff6a240707f9af71a0045d8997b4cd
Kole <novoselkosta@gmail.com>
2022-01-19 00:18:25 +0100

    Added IsLookable check similar to IsUsable

--- code/entities/interfaces/ILook.cs
+++ code/entities/interfaces/ILook.cs
@@ -1,7 +1,10 @@
-﻿namespace PaintBall;
+﻿using Sandbox;
+
+namespace PaintBall;
 
 public interface ILook
 {
+	public bool IsLookable( Entity viewer );
 	public void StartLook();
 	public void Update();
 	public void EndLook();
