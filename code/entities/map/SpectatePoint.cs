fe902a25292bdbdc81ba333b1700753b368d0dc0
Kole <novoselkosta@gmail.com>
2022-01-20 01:28:17 +0100

    Added Spectate Point

--- code/entities/map/SpectatePoint.cs
+++ code/entities/map/SpectatePoint.cs
@@ -3,8 +3,16 @@
 namespace PaintBall;
 
 [Library( "pb_spectatepoint" )]
-[Hammer.EntityTool("Spectate Point", "PaintBall", "Defines a fixed point where a player can spectate from." )]
-public partial class SpectatePoint : Entity
+[Hammer.EditorModel( "models/editor/camera.vmdl" )]
+[Hammer.EntityTool( "Spectate Point", "PaintBall", "Defines a fixed point where a player can spectate from." )]
+public class SpectatePoint : Entity
 {
+	[Property] public byte Index { get; set; }
 
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Transmit = TransmitType.Always;
+	}
 }
