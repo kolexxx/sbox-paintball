5f769219eb1b5f70c16abe98688aa40e480d685d
Kole <novoselkosta@gmail.com>
2022-01-20 01:30:09 +0100

    Markup for Map wrapper

---
+++ code/systems/Map.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PaintBall;
+
+public partial class Map : Entity
+{
+	public new string Name => Global.MapName;
+	public List<PlayerSpawnPoint> SpawnPoints { get; set; } = new();
+	public List<SpectatePoint> SpectatePoints { get; set; } = new();	
+	protected Output RoundStart { get; set;}
+	protected Output RounEnd { get; set; }
+	protected Output RoundNew { get; set; }
+
+	public Map()
+	{
+
+	}
+
+	[Event.Entity.PostSpawn]
+	private void EntityPostSpawn()
+	{
+		Debug.CheckRealms();
+
+		if ( Host.IsServer )
+		{
+			SpawnPoints = Entity.All.OfType<PlayerSpawnPoint>().ToList();
+		}
+
+		SpectatePoints = Entity.All.OfType<SpectatePoint>().ToList();
+	}
+}
