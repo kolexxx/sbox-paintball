f2ce70acb0d4d2f4959541bcd59835ddac962b62
Kole <novoselkosta@gmail.com>
2022-01-20 01:28:02 +0100

    Update PlayerSpawnPoint.cs

--- code/entities/map/PlayerSpawnPoint.cs
+++ code/entities/map/PlayerSpawnPoint.cs
@@ -1,12 +1,11 @@
-using Hammer;
 using Sandbox;
 
 namespace PaintBall;
 
 [Library( "pb_spawnpoint" )]
-[EditorModel( "models/editor/playerstart.vmdl", FixedBounds = true )]
-[EntityTool( "Player Spawnpoint", "PaintBall", "Defines a point where players on a team can spawn." )]
-public partial class PlayerSpawnPoint : Entity
+[Hammer.EditorModel( "models/editor/playerstart.vmdl", FixedBounds = true )]
+[Hammer.EntityTool( "Player Spawnpoint", "PaintBall", "Defines a point where players on a team can spawn." )]
+public class PlayerSpawnPoint : Entity
 {
 	[Property] public Team Team { get; set; }
 	public bool Occupied { get; set; }
