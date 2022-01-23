dde9ed53c14447541293158cd264e4cda683ff33
Kole <novoselkosta@gmail.com>
2022-01-23 16:39:46 +0100

    Now fires outputs

--- code/systems/Map.cs
+++ code/systems/Map.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PaintBall;
+namespace Paintball;
 
 [Library( "pb_map" )]
 public partial class Map : Entity
@@ -12,27 +12,47 @@ public partial class Map : Entity
 	public List<PlayerSpawnPoint> SpawnPoints { get; set; } = new();
 	public List<SpectatePoint> SpectatePoints { get; set; } = new();
 	protected Output RoundStart { get; set; }
-	protected Output RounEnd { get; set; }
+	protected Output RoundEnd { get; set; }
 	protected Output RoundNew { get; set; }
 
-	public Map()
-	{
-	}
+	public Map() { }
 
 	public async Task GetInfo()
 	{
 		Info = await Package.Fetch( Global.MapName, false );
-		Event.Run( PBEvent.Game.MapInfoFetched );
+
+		if ( Info != null )
+			Event.Run( PBEvent.Game.MapInfoFetched );
 	}
 
 	[Event.Entity.PostSpawn]
 	private void EntityPostSpawn()
 	{
+		GetInfo();
+
 		if ( Host.IsServer )
 		{
 			SpawnPoints = Entity.All.OfType<PlayerSpawnPoint>().ToList();
 		}
 
 		SpectatePoints = Entity.All.OfType<SpectatePoint>().ToList();
+	}
+
+	[PBEvent.Round.Start]
+	private void OnRoundStart()
+	{
+		RoundStart.Fire( this );
+	}
+
+	[PBEvent.Round.End]
+	private void OnRoundEnd( Team winner )
+	{
+		RoundEnd.Fire( this );
+	}
+
+	[PBEvent.Round.New]
+	private void OnNewRound()
+	{
+		RoundNew.Fire( this );
 	}
 }
