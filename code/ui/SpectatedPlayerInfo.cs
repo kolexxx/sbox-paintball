c172a63610af6c625186027e00a0c9f2225c5b70
Kole <novoselkosta@gmail.com>
2022-01-23 09:18:12 +0100

    Markup for SpectadedPlayerInfo

---
+++ code/ui/SpectatedPlayerInfo.cs
@@ -0,0 +1,32 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace PaintBall;
+
+[UseTemplate]
+public class SpectatedPlayerInfo : Panel
+{
+	private static SpectatedPlayerInfo s_current;
+	private Panel _playerInfo;
+	private Panel _spectatorControls;
+
+	public SpectatedPlayerInfo( Player player )
+	{
+	}
+
+	[PBEvent.Player.Spectating.Changed]
+	private static void OnSpectatedPlayerChanged( Player oldPlayer, Player newPlayer )
+	{
+		return;
+
+		if ( oldPlayer != null )
+			s_current.Delete();
+
+		if ( newPlayer == null )
+			return;
+
+		Local.Hud.AddChild( new SpectatedPlayerInfo( newPlayer ) );
+		s_current = Local.Hud.GetChild( Local.Hud.ChildrenCount - 1 ) as SpectatedPlayerInfo;
+	}
+}
