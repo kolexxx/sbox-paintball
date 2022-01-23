5b7f975eb78b8e9d8e799ea65a648251f74d684a
Kole <novoselkosta@gmail.com>
2022-01-23 16:34:58 +0100

    Added spectated player info

--- code/ui/SpectatedPlayerInfo.cs
+++ code/ui/SpectatedPlayerInfo.cs
@@ -1,29 +1,42 @@
 ﻿using Sandbox;
 using Sandbox.UI;
-using Sandbox.UI.Construct;
 
-namespace PaintBall;
+namespace Paintball;
 
 [UseTemplate]
 public class SpectatedPlayerInfo : Panel
 {
+	public Player TargetPlayer { get; set; }
+	public Panel PlayerInfo { get; set; }
+	public Image Avatar { get; set; }
+	public Label Name { get; set; }
+	public Panel SpectatorControls { get; set; }
 	private static SpectatedPlayerInfo s_current;
-	private Panel _playerInfo;
-	private Panel _spectatorControls;
 
 	public SpectatedPlayerInfo( Player player )
 	{
+		TargetPlayer = player;
+
+		SetClass( TargetPlayer.Team.GetString(), true );
+
+		Avatar.SetTexture( $"avatar:{TargetPlayer.Client.PlayerId}" );
+		Name.Text = TargetPlayer.Client.Name;
+
+		(SpectatorControls.GetChild( 0 ) as InputHint).SetButton( InputButton.Attack2 );
+		(SpectatorControls.GetChild( 0 ) as InputHint).Context.Text = "Switch to previous player";
+		(SpectatorControls.GetChild( 1 ) as InputHint).SetButton( InputButton.Jump );
+		(SpectatorControls.GetChild( 1 ) as InputHint).Context.Text = "Change spectate camera";
+		(SpectatorControls.GetChild( 2 ) as InputHint).SetButton( InputButton.Attack1 );
+		(SpectatorControls.GetChild( 2 ) as InputHint).Context.Text = "Switch to next player";
 	}
 
 	[PBEvent.Player.Spectating.Changed]
 	private static void OnSpectatedPlayerChanged( Player oldPlayer, Player newPlayer )
 	{
-		return;
-
-		if ( oldPlayer != null )
+		if ( oldPlayer != Local.Pawn )
 			s_current.Delete();
 
-		if ( newPlayer == null )
+		if ( newPlayer == Local.Pawn )
 			return;
 
 		Local.Hud.AddChild( new SpectatedPlayerInfo( newPlayer ) );
