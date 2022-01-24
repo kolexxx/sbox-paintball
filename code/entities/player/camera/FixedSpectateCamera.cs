a015f3710244dc059c4cec039c07a83588f275c5
Kole <novoselkosta@gmail.com>
2022-01-24 13:42:51 +0100

    Update FixedSpectateCamera.cs

--- code/entities/player/camera/FixedSpectateCamera.cs
+++ code/entities/player/camera/FixedSpectateCamera.cs
@@ -4,11 +4,33 @@ namespace Paintball;
 
 public class FixedSpectateCamera : Camera, ISpectateCamera
 {
+	private SpectatePoint _spectatePoint;
 	private int _index = 0;
+
+	public override void Activated()
+	{
+		base.Activated();
+
+		if ( Game.Current.Map.SpectatePoints.Count == 0 )
+			return;
+
+		_spectatePoint = Game.Current.Map.SpectatePoints[_index];
+	}
 
 	public override void Update()
 	{
-		throw new System.NotImplementedException();
+		if ( Input.Pressed( InputButton.Attack1 ) )
+			_index++;
+		else if ( Input.Pressed( InputButton.Attack2 ) )
+			_index--;
+
+		if ( _index < 0 )
+			_index = Game.Current.Map.SpectatePoints.Count - 1;
+		else if ( _index >= Game.Current.Map.SpectatePoints.Count )
+			_index = 0;
+
+		Rotation = _spectatePoint.Rotation;
+		Position = _spectatePoint.Position;
 	}
 
 	public void OnSpectatedPlayerChanged( Player oldPlayer, Player newPlayer ) { }
