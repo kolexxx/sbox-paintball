5d5394fde22420bbb07a493626ba0c6fcbb1446b
Kole <novoselkosta@gmail.com>
2022-01-26 20:01:45 +0100

    Markup for map select

---
+++ code/ui/general/mapselect/MapSelect.cs
@@ -0,0 +1,16 @@
+﻿using Sandbox.UI;
+
+namespace Paintball.UI;
+
+public class MapSelect : Panel
+{
+	public static MapSelect Instance;
+
+	public MapSelect()
+	{
+		Instance = this;
+
+		StyleSheet.Load( "/ui/general/mapselect/MapSelect.scss" );
+
+	}
+}
