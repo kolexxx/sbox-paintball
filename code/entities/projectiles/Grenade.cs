add8b17554577d32373aff448254602645985837
Kole <novoselkosta@gmail.com>
2022-01-27 21:22:52 +0100

    Buffed grenade

--- code/entities/projectiles/Grenade.cs
+++ code/entities/projectiles/Grenade.cs
@@ -8,48 +8,11 @@ namespace Paintball;
 public partial class Grenade : ModelEntity, ITeamEntity
 {
 	public RealTimeUntil DestroyTime { get; set; }
+	public int ProjectilesPer360 { get; set; } = 20;
 	public Entity Origin { get; set; }
 	public string ModelPath => "models/grenade/grenade_spent.vmdl";
 	public Team Team { get; set; }
 	public float LifeTime => 3f;
-
-	private static float _sqrt2over2 = (float)Math.Sqrt( 2 ) / 2f;
-	// ATTENTION! THIS IS REALLY SHIT! THIS CAN BE DONE USING A SIMPLE FOR LOOP WITH ORIENTATION!
-	private static readonly Vector3[] s_directions =
-	{
-			Vector3.Left,
-			Vector3.Right,
-			Vector3.Backward,
-			Vector3.Forward,
-			new Vector3(_sqrt2over2,-_sqrt2over2, 0),
-			new Vector3(_sqrt2over2,_sqrt2over2, 0),
-			new Vector3(-_sqrt2over2,_sqrt2over2, 0),
-			new Vector3(-_sqrt2over2,-_sqrt2over2, 0),
-			new Vector3((float)Math.Sin(Math.PI/8),-(float)Math.Cos(Math.PI/8), 0),
-			new Vector3((float)Math.Sin(Math.PI/8),(float)Math.Cos(Math.PI/8), 0),
-			new Vector3(-(float)Math.Sin(Math.PI/8),(float)Math.Cos(Math.PI/8), 0),
-			new Vector3(-(float)Math.Sin(Math.PI/8),-(float)Math.Cos(Math.PI/8), 0),
-			new Vector3((float)Math.Cos(Math.PI/8),-(float)Math.Sin(Math.PI/8), 0),
-			new Vector3((float)Math.Cos(Math.PI/8),(float)Math.Sin(Math.PI/8), 0),
-			new Vector3(-(float)Math.Cos(Math.PI/8),(float)Math.Sin(Math.PI/8), 0),
-			new Vector3(-(float)Math.Cos(Math.PI/8),-(float)Math.Sin(Math.PI/8), 0),
-			new Vector3((float)Math.Sin(Math.PI/8),-(float)Math.Cos(Math.PI/8), _sqrt2over2/4),
-			new Vector3((float)Math.Sin(Math.PI/8),(float)Math.Cos(Math.PI/8), _sqrt2over2/4),
-			new Vector3(-(float)Math.Sin(Math.PI/8),(float)Math.Cos(Math.PI/8), _sqrt2over2/4),
-			new Vector3(-(float)Math.Sin(Math.PI/8),-(float)Math.Cos(Math.PI/8), _sqrt2over2/4),
-			new Vector3((float)Math.Cos(Math.PI/8),-(float)Math.Sin(Math.PI/8), _sqrt2over2/4),
-			new Vector3((float)Math.Cos(Math.PI/8),(float)Math.Sin(Math.PI/8), _sqrt2over2/4),
-			new Vector3(-(float)Math.Cos(Math.PI/8),(float)Math.Sin(Math.PI/8), _sqrt2over2/4),
-			new Vector3(-(float)Math.Cos(Math.PI/8),-(float)Math.Sin(Math.PI/8), _sqrt2over2/4),
-			new Vector3(_sqrt2over2,-_sqrt2over2, _sqrt2over2/4),
-			new Vector3(_sqrt2over2,_sqrt2over2, _sqrt2over2/4),
-			new Vector3(-_sqrt2over2,_sqrt2over2, _sqrt2over2/4),
-			new Vector3(-_sqrt2over2,-_sqrt2over2, _sqrt2over2/4),
-			Vector3.Left + new Vector3(0, 0, _sqrt2over2/4),
-			Vector3.Right + new Vector3(0, 0, _sqrt2over2/4),
-			Vector3.Backward + new Vector3(0, 0, _sqrt2over2/4),
-			Vector3.Forward + new Vector3(0, 0, _sqrt2over2/4)
-		};
 
 	public override void Spawn()
 	{
@@ -80,24 +43,29 @@ public partial class Grenade : ModelEntity, ITeamEntity
 		if ( Owner is not Player owner )
 			return;
 
-		foreach ( var direction in s_directions )
+		for ( float pitch = 0; pitch >= -45; pitch -= 15)
 		{
-			var projectile = new BaseProjectile()
+			for ( float yaw = 0; yaw <= 360; yaw += 360 / ProjectilesPer360 )
 			{
-				Owner = owner,
-				Team = Team,
-				FollowEffect = $"particles/{owner.Team.GetString()}_glow.vpcf",
-				HitSound = "impact",
-				Scale = 0.25f,
-				Gravity = 0f,
-				ModelPath = $"models/{owner.Team.GetString()}_ball/ball.vmdl",
-				IsServerOnly = true,
-				Origin = Origin
-			};
+				var angles = new Angles( pitch, yaw, 0 );
 
-			var velocity = direction * 2500f;
+				var projectile = new BaseProjectile()
+				{
+					Owner = owner,
+					Team = Team,
+					FollowEffect = $"particles/{owner.Team.GetString()}_glow.vpcf",
+					HitSound = "impact",
+					Scale = 0.25f,
+					Gravity = 10f,
+					ModelPath = $"models/{owner.Team.GetString()}_ball/ball.vmdl",
+					IsServerOnly = true,
+					Origin = Origin
+				};
 
-			projectile.Initialize( PhysicsBody.MassCenter, velocity, 4f );
+				var velocity = Rotation.From( angles ).Forward * 1000f;
+
+				projectile.Initialize( PhysicsBody.MassCenter, velocity, 4f );
+			}
 		}
 	}
 }
