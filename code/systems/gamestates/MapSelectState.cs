ca67a7de194e94975c7dd1662c6ccc8adfdd0eb0
Kole <novoselkosta@gmail.com>
2022-01-27 12:27:40 +0100

    Markup for Map Selection

---
+++ code/systems/gamestates/MapSelectState.cs
@@ -0,0 +1,88 @@
+﻿using Paintball.UI;
+using Sandbox;
+using System.Collections.Generic;
+
+namespace Paintball;
+
+public partial class MapSelectState : BaseState
+{
+	public override int StateDuration => 15;
+	[Net, Change] public IDictionary<string, string> MapImages { get; set; }
+	[Net] public IDictionary<long, string> PlayerIdVote { get; set; }
+	[Net] public IDictionary<string, int> VoteCount { get; set; }
+
+	public override void OnPlayerLeave( Player player )
+	{
+		base.OnPlayerLeave( player );
+
+		VoteCount[PlayerIdVote[player.Client.PlayerId]]--;
+		PlayerIdVote.Remove( player.Client.PlayerId ); // I don't think this is needed
+	}
+
+	public override void OnSecond()
+	{
+		base.OnSecond();
+
+		if ( Host.IsServer && UntilStateEnds )
+			Game.Current.ChangeState( new WaitingForPlayersState() );
+	}
+
+	public override void Start()
+	{
+		base.Start();
+
+		if ( !Host.IsServer )
+			return;
+
+		foreach ( var player in Players )
+			player.Inventory?.DeleteContents();
+
+		VoteCount = new Dictionary<string, int>();
+		PlayerIdVote = new Dictionary<long, string>();
+	}
+
+	public override void TimeUp()
+	{
+		base.TimeUp();
+
+		if ( !Host.IsServer )
+			return;
+
+		string mostVotedMap = string.Empty;
+		int count = int.MinValue;
+
+		foreach ( KeyValuePair<string, int> kvp in VoteCount )
+		{
+			if ( kvp.Value > count )
+			{
+				mostVotedMap = kvp.Key;
+				count = kvp.Value;
+			}
+		}
+
+		Global.ChangeLevel( mostVotedMap );
+	}
+
+	[ServerCmd]
+	public static void SetVote( string map )
+	{
+		Assert.NotNull( map );
+
+		var player = ConsoleSystem.Caller.Pawn as Player;
+
+		if ( !player.IsValid() )
+			return;
+
+		var state = Game.Current.State as MapSelectState;
+		string oldMapVote = state.PlayerIdVote[player.Client.PlayerId];
+
+		if ( oldMapVote == map )
+			return;
+
+		if ( oldMapVote != null )
+			state.VoteCount[oldMapVote]--;
+
+		state.PlayerIdVote[player.Client.PlayerId] = map;
+		state.VoteCount[map]++;
+	}
+}
--- code/ui/general/mapselect/MapSelect.cs
+++ code/ui/general/mapselect/MapSelect.cs
@@ -1,25 +1,55 @@
 ﻿using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Paintball.UI;
 
 public class MapSelect : Panel
 {
 	public static MapSelect Instance;
+	private ProgressBar _progressBar;
 
 	public MapSelect()
 	{
 		Instance = this;
 
 		StyleSheet.Load( "/ui/general/mapselect/MapSelect.scss" );
+
+		AddChild( new ProgressBar( () =>
+		{
+			var state = Game.Current.State;
+
+			return 1 - state.UntilStateEnds.Relative / state.StateDuration;
+		} ) );
+
+		_progressBar = GetChild( ChildrenCount - 1 ) as ProgressBar;
 	}
 
 	[PBEvent.Game.StateChanged]
 	public static void OnStateChanged( BaseState _, BaseState newState )
 	{
-		if ( !Host.IsClient || newState is not GameFinishedState )
+		if ( !Host.IsClient || newState is not MapSelectState )
 			return;
 
-		Local.Hud.AddChild<MapSelect>();
+		Local.Hud.AddChild<MapSelect>(); ;
+	}
+
+	public sealed class Entry : Panel
+	{
+		public Label Title;
+
+		public Entry( string title, string thumbnail )
+		{
+			Title = Add.Label( title, "title" );
+
+			Style.BackgroundImage = Texture.Load( thumbnail );
+
+			AddEventListener( "onclick()", () =>
+			{
+				MapSelectState.SetVote( title );
+			} );
+		}
 	}
 }
