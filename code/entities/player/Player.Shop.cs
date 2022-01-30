a8e02c590817f374cec846632292f438673e2461
Kole <novoselkosta@gmail.com>
2022-01-30 14:02:20 +0100

    Initial markup for the shop

---
+++ code/entities/player/Player.Shop.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox;
+using System;
+
+namespace Paintball;
+
+public partial class Player
+{
+	[Net] public int Money { get; set; } = 1000;
+
+	[ServerCmd]
+	public static void RequestItem( string typeName )
+	{
+		if ( Game.Current.State is not GameplayState state )
+			return;
+
+		if ( state.RoundState != RoundState.Freeze || string.IsNullOrEmpty( typeName ) )
+			return;
+
+		Type type = Type.GetType( typeName );
+		var ent = Library.Create<Entity>(typeName);
+	}
+}
+
