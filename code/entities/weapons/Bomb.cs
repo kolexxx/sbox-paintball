3a05f00e987c8939e03ab8486215b19a9c977f5e
Kole <novoselkosta@gmail.com>
2022-02-07 11:05:33 +0100

    Update Bomb.cs

--- code/entities/weapons/Bomb.cs
+++ code/entities/weapons/Bomb.cs
@@ -2,20 +2,20 @@
 
 namespace Paintball;
 
-[Library( "pb_bomb", Title = "Bomb", Description = "A bomb that can be planted on a bombsite.", Spawnable = false )]
+[Item( SlotType.Deployable, Team.Red )]
+[Library( "pb_bomb", Title = "Bomb", Description = "A bomb that can be planted on a bombsite.", Icon = "ui/weapons/bomb.png", Spawnable = false )]
 [Hammer.EditorModel( "weapons/rust_shotgun/rust_shotgun.vmdl" )]
+[Hammer.Skip]
 public sealed partial class Bomb : Weapon
 {
 	[Net, Predicted] public TimeSince Delay { get; set; } = 2f;
 	[Net] public TimeSince TimeSinceStartedPlanting { get; set; } = 0f;
+	public override ItemConfig Config { get; set; } 
 	public override bool Automatic => true;
 	public override int ClipSize => 1;
-	public override Team ExclusiveFor => Team.Red;
 	public override string FireSound => "";
-	public override string Icon => "ui/weapons/bomb.png";
 	public override float PrimaryRate => 0;
 	public override float ReloadTime => 2.0f;
-	public override SlotType Slot => SlotType.Deployable;
 	public override string ViewModelPath => "weapons/rust_shotgun/v_rust_shotgun.vmdl";
 
 	public override void ActiveStart( Entity entity )
@@ -43,7 +43,10 @@ public sealed partial class Bomb : Weapon
 
 	public override bool CanPrimaryAttack()
 	{
-		if ( Owner.CanPlantBomb && base.CanPrimaryAttack() && Delay >= 2f )
+		if ( !Owner.CanPlantBomb )
+			return false;
+
+		if ( base.CanPrimaryAttack() && Delay >= 2f )
 		{
 			if ( !Owner.IsPlantingBomb )
 			{
