ad648a6b6a061e76f297f16e3b87df1be372d9d9
Kole <novoselkosta@gmail.com>
2022-02-07 10:51:11 +0100

    Create PanelExtensions.cs

---
+++ code/util/PanelExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Sandbox.UI;
+
+namespace Paintball;
+public static class PanelExtensions
+{
+	public static Panel LastChild(this Panel panel )
+	{
+		return panel.GetChild( panel.ChildrenCount - 1 );
+	}
+}
