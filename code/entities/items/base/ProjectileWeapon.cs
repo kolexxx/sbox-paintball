7379b9a22055c3c0ceaa1834598817c5160d0790
Kole <novoselkosta@gmail.com>
2022-02-13 16:06:07 +0100

    Added ProjectileWeaponInfo that inherits from CarriableInfo

--- code/entities/items/base/ProjectileWeapon.cs
+++ code/entities/items/base/ProjectileWeapon.cs
@@ -1,20 +1,78 @@
 ﻿using Sandbox;
+using System.ComponentModel;
 
 namespace Paintball;
 
+public enum FireMode
+{
+	Automatic,
+	Semi,
+	Burst
+}
+
+[Library( "pwpn" ), AutoGenerate]
+public partial class ProjectileWeaponInfo : CarriableInfo
+{
+	[Property, Category( "VFX" ), ResourceType( "vpcf" )] public string FollowEffect { get; set; }
+	[Property, Category( "Sounds" )] public string FireSound { get; set; }
+	[Property, Category( "Models" ), ResourceType( "vmdl" )] public string ProjectileModel { get; set; } = "models/paintball/paintball.vmdl";
+	[Property, Category( "Stats" )] public int ClipSize { get; set; }
+	[Property, Category( "Stats" )] public int BulletsPerFire { get; set; } = 1;
+	[Property, Category( "Stats" )] public FireMode FireMode { get; set; }
+	[Property, Category( "Stats" )] public float PrimaryRate { get; set; } = 5f;
+	[Property, Category( "Stats" )] public float ProjectileGravity { get; set; }
+	[Property, Category( "Stats" )] public float ProjectileRadius { get; set; } = 3f;
+	[Property, Category( "Stats" )] public float ProjectileScale { get; set; } = 0.25f;
+	[Property, Category( "Stats" )] public float ProjectileSpeed { get; set; } = 2000f;
+	[Property, Category( "Stats" )] public float ReloadTime { get; set; } = 5f;
+	[Property, Category( "Stats" )] public int ReserveAmmo { get; set; }
+	[Property, Category( "Stats" )] public float SecondaryRate { get; set; }
+	[Property, Category( "Stats" )] public float Spread { get; set; } = 0f;
+}
+
 [Hammer.Skip]
-public abstract partial class ProjectileWeapon<T> : Weapon where T : BaseProjectile, new()
+public abstract partial class ProjectileWeapon<T> : Carriable where T : BaseProjectile, new()
 {
 	public virtual int BulletsPerFire => 1;
 	public virtual string FollowEffect => $"particles/{Owner.Team.GetTag()}_glow.vpcf";
 	public virtual string HitSound => "impact";
-	public virtual string ProjectileModel => $"models/paintball/paintball.vmdl";
+	public new ProjectileWeaponInfo Info
+	{
+		get => base.Info as ProjectileWeaponInfo;
+		set => base.Info = value;
+	}
+	public virtual string ProjectileModel => "models/paintball/paintball.vmdl";
 	public virtual float ProjectileGravity => 0f;
 	public virtual float ProjectileRadius => 3f;
 	public virtual float ProjectileScale => 0.25f;
 	public virtual float ProjectileSpeed => 2000f;
 	public virtual float Spread => 0f;
 	public override string ViewModelPath => "weapons/rust_pistol/v_rust_pistol.vmdl";
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		AmmoClip = Info.ClipSize;
+		ReserveAmmo = Info.ReserveAmmo;
+	}
+
+	public override bool CanPrimaryAttack()
+	{
+		if ( Owner.IsFrozen )
+			return false;
+
+		if ( Info.FireMode == FireMode.Semi && !Input.Pressed( InputButton.Attack1 ) )
+			return false;
+		else if ( Info.FireMode == FireMode.Automatic && !Input.Down( InputButton.Attack1 ) )
+			return false;
+
+		var rate = Info.PrimaryRate;
+		if ( rate <= 0 )
+			return true;
+
+		return TimeSincePrimaryAttack > (1 / rate);
+	}
 
 	public override void AttackPrimary()
 	{
@@ -38,7 +96,7 @@ public abstract partial class ProjectileWeapon<T> : Weapon where T : BaseProject
 		Owner.SetAnimBool( "b_attack", true );
 
 		ShootEffects();
-		PlaySound( FireSound );
+		PlaySound( Info.FireSound );
 
 		if ( Prediction.FirstTime )
 		{
@@ -62,9 +120,9 @@ public abstract partial class ProjectileWeapon<T> : Weapon where T : BaseProject
 			Team = owner.Team,
 			// FollowEffect = FollowEffect,
 			// HitSound = HitSound,
-			Scale = ProjectileScale,
-			Radius = ProjectileRadius,
-			Gravity = ProjectileGravity,
+			Scale = Info.ProjectileScale,
+			Radius = Info.ProjectileRadius,
+			Gravity = Info.ProjectileGravity,
 			Simulator = owner.Projectiles,
 			// ModelPath = ProjectileModel,
 			Rotation = owner.EyeRotation,
@@ -97,9 +155,9 @@ public abstract partial class ProjectileWeapon<T> : Weapon where T : BaseProject
 					Team = owner.Team,
 					// FollowEffect = FollowEffect,
 					// HitSound = HitSound,
-					Scale = ProjectileScale,
-					Radius = ProjectileRadius,
-					Gravity = ProjectileGravity,
+					Scale = Info.ProjectileScale,
+					Radius = Info.ProjectileRadius,
+					Gravity = Info.ProjectileGravity,
 					Simulator = owner.Projectiles,
 					// ModelPath = ProjectileModel,
 					Rotation = owner.EyeRotation,
