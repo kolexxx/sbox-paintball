d5c9ddd7efcd64590822034bd5dbdff67730f233
Kole <novoselkosta@gmail.com>
2022-02-16 21:15:34 +0100

    Add List shuffle extension

---
+++ code/util/ListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace Paintball;
+
+public static class ListExtensions
+{
+	public static void Shuffle<T>( this IList<T> list )
+	{
+		int n = list.Count;
+		while ( n > 1 )
+		{
+			n--;
+			int k = Rand.Int( 0, n );
+			T value = list[k];
+			list[k] = list[n];
+			list[n] = value;
+		}
+	}
+
+}
